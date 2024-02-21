7c394d98d758d95b2994d728a0e36773d6271bc1
swizzlejuice <bumblefizzzle@gmail.com>
2024-02-20 18:22:59 -0800

    update

--- caredontcareapp/src/App.js
+++ caredontcareapp/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './index.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { getDatabase, ref, set as firebaseSet, push as firebasePush, onValue } from 'firebase/database';
 
 import { SignupForm } from './Login.js';
 import { ProfilePage } from './Profile.js';
 
-export default function App() {
+
+export default function App(props) {
   return (
     <div>
       <Routes>
--- caredontcareapp/src/index.js
+++ caredontcareapp/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { getAuth } from "firebase/auth";
 import './index.css';
 
 import App from './App';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCYhYk8Qg5Nd2PNqrq-_pJ3JLRBk3ZqWfU",
+  authDomain: "caredontcarenews.firebaseapp.com",
+  projectId: "caredontcarenews",
+  storageBucket: "caredontcarenews.appspot.com",
+  messagingSenderId: "1076140224134",
+  appId: "1:1076140224134:web:1b2bdc7e857d08ebf32b07"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
